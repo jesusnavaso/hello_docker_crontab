7d9a1d20e97a2ff88d0b371d3eaf7e69856b0838
jesusnavaso <74953560+jesusnavaso@users.noreply.github.com>
2022-02-05 13:02:16 +0100

    Initial commit with basic project files

---
+++ hello_docker.py
@@ -0,0 +1,21 @@
+import logging
+from pathlib import Path
+
+
+def log_something():
+    logger.info("Hello Docker, this is a test log.")
+
+
+if __name__ == '__main__':
+    LOG_FOLDER = Path("./logs")
+    LOG_FOLDER.mkdir(exist_ok=True)
+    LOG_FILE_PATH = Path(LOG_FOLDER, "hello_docker.log")
+
+    logger = logging.getLogger("hello_docker_class")
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(LOG_FILE_PATH)
+    log_formatter = logging.Formatter(f"%(asctime)s\t%(levelname)s - {__file__} : %(message)s")
+    file_handler.setFormatter(log_formatter)
+    logger.addHandler(file_handler)
+
+    log_something()
